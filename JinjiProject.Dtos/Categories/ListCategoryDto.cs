a01edf4ec9b3c984c1391a919f8fed1ed5073c16
Eren Çolak <eren.colk01@gmail.com>
2024-02-28 12:29:00 +0300

    Harddeletede modal eklendi

--- JinjiProject.Dtos/Categories/ListCategoryDto.cs
+++ JinjiProject.Dtos/Categories/ListCategoryDto.cs
@@ -12,6 +12,10 @@ namespace JinjiProject.Dtos.Categories
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public DateTime DeletedDate{ get; set; }
+        public DateTime CreatedDate{ get; set; }
+        public DateTime ModifiedDate{ get; set; }
+
         public Status Status{ get; set; }
 
     }
