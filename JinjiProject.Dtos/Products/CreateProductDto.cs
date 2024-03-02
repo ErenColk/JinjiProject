1f18140fafc4ea42e4cad8423d01cffea28458c6
Eren Çolak <eren.colk01@gmail.com>
2024-03-02 22:44:04 +0300

    Manager bitti

--- JinjiProject.Dtos/Products/CreateProductDto.cs
+++ JinjiProject.Dtos/Products/CreateProductDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,8 @@ namespace JinjiProject.Dtos.Products
         public int Price { get; set; }
         public int Stock { get; set; }
         public string ImagePath { get; set; }
+        public IFormFile UploadPath { get; set; }
+
         public int? Capacity { get; set; }
     }
 }
