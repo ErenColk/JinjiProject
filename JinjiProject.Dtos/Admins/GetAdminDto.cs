bfb3681f819df758ef9490527aadcd6915f3e931
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2024-03-03 15:59:42 +0300

    admin güncelleme tamamlandı

--- JinjiProject.Dtos/Admins/GetAdminDto.cs
+++ JinjiProject.Dtos/Admins/GetAdminDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JinjiProject.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,11 @@ namespace JinjiProject.Dtos.Admins
 {
     public class GetAdminDto
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
--- JinjiProject.UI/Areas/Admin/Controllers/AdminController.cs
+++ JinjiProject.UI/Areas/Admin/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using JinjiProject.BusinessLayer.Managers.Abstract;
+using JinjiProject.BusinessLayer.Validator.AdminValidations;
+using JinjiProject.BusinessLayer.Validator.CategoryValidations;
 using JinjiProject.Core.Entities.Concrete;
 using JinjiProject.Core.Enums;
 using JinjiProject.Core.Utilities.Results.Concrete;
@@ -24,6 +27,20 @@ namespace JinjiProject.UI.Areas.Admin.Controllers
         public async Task<IActionResult> AdminList()
         {
             var result = await adminService.GetAllByExpression(admin => admin.Status != Status.Deleted);
+            var softDeleteSuccess = TempData["SoftDeleteSuccess"];
+            var softDeleteError = TempData["SoftDeleteError"];
+            var hardDeleteSuccess = TempData["HardDeleteSuccess"];
+            var hardDeleteError = TempData["HardDeleteError"];
+            ViewBag.Errors = TempData["CreateAdminError"];
+            if (softDeleteSuccess != null)
+                NotifySuccess($"{softDeleteSuccess}");
+            else if (softDeleteError != null)
+                NotifyError($"{softDeleteError}");
+            else if (hardDeleteSuccess != null)
+                NotifySuccess($"{hardDeleteSuccess}");
+            else if (hardDeleteError != null)
+                NotifyError($"{hardDeleteError}");
+
             if (result.IsSuccess)
             {
                 var adminList = mapper.Map<IEnumerable<ListAdminDto>>(result.Data);
@@ -39,25 +56,118 @@ namespace JinjiProject.UI.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateAdmin(CreateAdminDto createAdminDto)
         {
-
-            var admin = await adminService.CreateAdminAsync(createAdminDto);
-            if (admin.IsSuccess)
+            CreateAdminValidator validations = new CreateAdminValidator();
+            var result = validations.Validate(createAdminDto);
+            if(result.IsValid)
             {
-                NotifySuccess(admin.Message);
+                var admin = await adminService.CreateAdminAsync(createAdminDto);
+                if (admin.IsSuccess)
+                {
+                    NotifySuccess(admin.Message);
+                }
+                else
+                {
+                    NotifyError(admin.Message);
+                }
             }
             else
             {
-                NotifyError(admin.Message);
+                var errorsString = "";
+                foreach (var item in result.Errors)
+                {
+                    errorsString += "<li>"+item.ErrorMessage+"</li>";
+                }
+                TempData["CreateAdminError"] = errorsString;
             }
+            
             return RedirectToAction("AdminList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateAdmin(int id)
+        {
+            var updateAdminResult = await adminService.GetAdminById(id);
+            if (updateAdminResult.IsSuccess)
+            {
+                UpdateAdminDto updateAdmin = mapper.Map<UpdateAdminDto>(updateAdminResult.Data);
+                return View(updateAdmin);
+
+            }
+            else
+            {
+                UpdateAdminDto updateAdmin = mapper.Map<UpdateAdminDto>(updateAdminResult.Data);
+                NotifyError(updateAdminResult.Message);
+                return RedirectToAction(nameof(AdminList));
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAdmin(UpdateAdminDto updateAdminDto)
+        {
+            UpdateAdminValidator updateAdminValidator = new UpdateAdminValidator();
+            var result = updateAdminValidator.Validate(updateAdminDto);
+
+            if (result.IsValid)
+            {
+                var updateAdminResult = await adminService.UpdateAdminAsync(updateAdminDto);
+
+                if (updateAdminResult.IsSuccess)
+                {
+
+                    NotifySuccess(updateAdminResult.Message);
+
+                }
+                else
+                {
+                    NotifyError(updateAdminResult.Message);
+                }
+
+                return RedirectToAction(nameof(AdminList));
+            }
+
+            foreach (var item in result.Errors)
+            {
+                if (item.ErrorCode == "1")
+                {
+                    ViewData["FirstName"] += item.ErrorMessage + "\n";
+                }
+                else if (item.ErrorCode == "2") 
+                {
+                    ViewData["LastName"] += item.ErrorMessage + "\n";
+                }
+                else if(item.ErrorCode == "3")
+                {
+                    ViewData["BirthDate"] += item.ErrorMessage + "\n";
+                }
+                else if (item.ErrorCode == "4")
+                {
+                    ViewData["Gender"] += item.ErrorMessage + "\n";
+                }
+                else if (item.ErrorCode == "5")
+                {
+                    ViewData["Email"] += item.ErrorMessage + "\n";
+                }
+                else if (item.ErrorCode == "6")
+                {
+                    ViewData["UploadPath"] += item.ErrorMessage + "\n";
+                }
+            }
+            return View(updateAdminDto);
         }
 
         [HttpGet]
         public async Task<IActionResult> SoftDelete(int id)
         {
 
-            await adminService.SoftDeleteAdminAsync(id);
-
+            var softDeleteAdmin = await adminService.SoftDeleteAdminAsync(id);
+            if (softDeleteAdmin.IsSuccess)
+            {
+                TempData["SoftDeleteSuccess"] = softDeleteAdmin.Message;
+            }
+            else
+            {
+                TempData["SoftDeleteError"] = softDeleteAdmin.Message;
+            }
             return RedirectToAction(nameof(AdminList));
         }
 
@@ -67,7 +177,15 @@ namespace JinjiProject.UI.Areas.Admin.Controllers
         public async Task<IActionResult> HardDelete(int id)
         {
 
-            await adminService.HardDeleteAdminAsync(id);
+            var hardDeleteAdmin = await adminService.HardDeleteAdminAsync(id);
+            if (hardDeleteAdmin.IsSuccess)
+            {
+                TempData["HardDeleteSuccess"] = hardDeleteAdmin.Message;
+            }
+            else
+            {
+                TempData["HardDeleteError"] = hardDeleteAdmin.Message;
+            }
             return RedirectToAction(nameof(AdminList));
         }
 
