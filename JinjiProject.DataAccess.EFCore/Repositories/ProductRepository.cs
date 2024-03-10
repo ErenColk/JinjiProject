1d8383fa69295a03f572924abf412d6cbbc1c239
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2024-03-10 18:49:49 +0300

    filtre dinamik yapıldı

--- JinjiProject.DataAccess.EFCore/Repositories/ProductRepository.cs
+++ JinjiProject.DataAccess.EFCore/Repositories/ProductRepository.cs
@@ -1,9 +1,11 @@
 ﻿using JinjiProject.Core.Entities.Concrete;
 using JinjiProject.DataAccess.Interface.Repositories;
 using JinjiProject.DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +13,17 @@ namespace JinjiProject.DataAccess.EFCore.Repositories
 {
     public class ProductRepository : BaseRepository<Product>, IProductRepository
     {
+        private readonly AppDbContext appDbContext;
+
         public ProductRepository(AppDbContext appDbContext) : base(appDbContext)
         {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllIncludeByExpression(Expression<Func<Product, bool>> expression)
+        {
+            var products = await appDbContext.Products.Where(expression).Include(x => x.Genre).ThenInclude(x => x.Category).ToListAsync();
+            return products;
         }
     }
 }
