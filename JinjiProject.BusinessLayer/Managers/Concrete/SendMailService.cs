d436e65227e06784913eb4ce545da5ff5a748208
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2024-03-23 14:07:30 +0300

    mail düzeltildi

--- JinjiProject.BusinessLayer/Managers/Concrete/SendMailService.cs
+++ JinjiProject.BusinessLayer/Managers/Concrete/SendMailService.cs
@@ -117,13 +117,23 @@ namespace JinjiProject.BusinessLayer.Managers.Concrete
             line-height: 1.6;
             color: #333;
         }}
+        .card-container {{display: flex;
+    overflow-y: auto;
+    max-height: 600px; 
+    justify-content: space-between;
+  }}
         .container {{
             max-width: 600px;
             margin: 0 auto;
             padding: 20px;
         }}
         .product {{
-            margin-bottom: 10px;
+    padding: 20px;
+    margin: 10px 0;
+    border-bottom: 1px solid #ccc;
+    border: 1px solid;
+    border-radius: 10px;
+    margin-right:10px;
         }}
         .product-name {{
             font-weight: bold;
@@ -154,7 +164,7 @@ namespace JinjiProject.BusinessLayer.Managers.Concrete
 <body>
     <div class='container'>
         <p>Merhaba,</p>
-        <p>Size ürünler hakkında bir güncelleme yapmak istiyoruz. Aşağıda bulunan ürünler şu anda indirime girdi! İndirimli fiyatlarımızı kaçırmayın.</p>";
+        <p>Size ürünler hakkında bir güncelleme yapmak istiyoruz. Aşağıda bulunan ürünler şu anda indirime girdi! İndirimli fiyatlarımızı kaçırmayın.</p><div class=""card-container"">";
 
                 foreach (var productDto in listProductDtos)
                 {                  
@@ -166,12 +176,13 @@ namespace JinjiProject.BusinessLayer.Managers.Concrete
         <div class='product'>
             <img style='max-width:100px; max-height:100px;' src='cid:{image.ContentId}' />
             <p class='product-name'><strong>{productDto.Name}</strong></p>
-            <p><span class='product-price'>{productDto.Price}₺</span> <span class='product-old-price'>{productDto.OldPrice}₺</span> <a class='product-link' href='{urL + '/' + productDto.Id}'>Ürüne Git</a></p>
+            <p><span class='product-price'>{productDto.Price}₺</span> <span class='product-old-price'>{productDto.OldPrice}₺</span></p>
+            <a class='product-link' href='{urL + '/' + productDto.Id}'>Ürüne Git</a>
         </div>";
                     }
                 }
 
-                htmlContent += $@"
+                htmlContent += $@"</div>
         <p>Ürünlere gitmek için yanlarındaki bağlantıya tıklayabilirsiniz.</p>
         <p>Fırsatları kaçırmayın!</p>
         <p class='signature'>Saygılarımla,<br>
